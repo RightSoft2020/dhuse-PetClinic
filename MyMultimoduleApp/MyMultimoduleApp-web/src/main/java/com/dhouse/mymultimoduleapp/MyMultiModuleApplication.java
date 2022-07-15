42c75dd3fafd612fa6f6b3fd7adc19d0478c5185
RightSoft2020 <right.soft@yahoo.com>
2022-07-15 22:55:15 +0400

    create and set up project as multi module maven project. closes #1

---
+++ MyMultimoduleApp/MyMultimoduleApp-web/src/main/java/com/dhouse/mymultimoduleapp/MyMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.dhouse.mymultimoduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMultiModuleApplication.class, args);
+    }
+
+}
---
+++ MyMultimoduleApp/MyMultimoduleApp-web/src/test/java/com/dhouse/mymultimoduleapp/MyMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhouse.mymultimoduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
