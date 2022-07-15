0f3ca1763c0e2d94909e813acb88276f6c8fb905
RightSoft2020 <right.soft@yahoo.com>
2022-07-15 18:04:00 +0400

    project is set up

---
+++ dhuse-PetClinic/src/main/java/com/example/dhusepetclinic/DhusePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.dhusepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhusePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhusePetClinicApplication.class, args);
+    }
+
+}
---
+++ dhuse-PetClinic/src/test/java/com/example/dhusepetclinic/DhusePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dhusepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhusePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
